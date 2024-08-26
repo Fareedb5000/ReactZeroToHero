681fa8ac606539333812b50139677fe3e2d58626
Fareed Bello <fareedb5000@gmail.com>
2024-08-26 11:10:34 +0100

    React Learning

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Profile from './components/Profile';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello, React with Typescript!</h1>
+            <Profile></Profile>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Profile = () =>{
+
+    return(
+        <div>
+            <h1>Hello, My name is Fareed Bello</h1>
+            <p>I am a programmer</p>
+        </div>
+    )
+}
+export default Profile;
